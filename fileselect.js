0df820820b5b20a535522697276f04a7849b7495
ivan <ivy3595@gmail.com>
2016-09-17 21:27:36 +0200

    added dynamic heading to index page, fixed various other issues which tend to escape me right this moment, but  I must have been with something cause God knows I have been pounding away at this son of a bitch all bloody day, and it was such a nice day too :-(

--- fileselect.js
+++ fileselect.js
@@ -1,103 +1,88 @@
 
-	var theSrc = "";
-	var theTag = "";
-	var imgDelList = "";
-	var tagList = "";
-	var imgname = "";
+var theSrc = "";
+var theTag = "";
+var imgDelList = "";
+var tagList = "";
+var imgname = "";
 function handleFileSelect(evt) {
-		var files = evt.target.files; 
-		$("#optionsDsplay").slideUp('fast');
-		$('#imgDisplay').html("");
-		  $('#newtagslist').val("");
-		for (var i = 0, f; f = files[i]; i++) {
-	  	if (!f.type.match('image.*')) {
-	 	 	continue;
+	var files = evt.target.files; 
+	$("#optionsDsplay").slideUp('fast');
+	$('#imgDisplay').html("");
+	$('#newtagslist').val("");
+	for (var i = 0, f; f = files[i]; i++) {
+		if (!f.type.match('image.*')) {
+		 	continue;
 	 	}
 	 	if (i > 5){
-	 	 alert("Select up to 6 media files");
-	 	 continue;
-	 	}
+			alert("Select up to 6 media files");
+			continue;
+		}
 		var reader = new FileReader();
 		reader.onload = (function(theFile) {
-	  return function(e) {
-		$('#imgDisplay').append('<img class="thumbpic" src="'+ e.target.result + '" title="'+ escape(theFile.name)+ '" onclick="showOps(this)" /\>');
-	  };
-	})(f);
-	reader.readAsDataURL(f);
-      }
-      $('#newtagslist').html("");
-    }
-    
+			return function(e) {
+				$('#imgDisplay').append('<img class="thumbpic" src="'+ e.target.result + '" title="'+ escape(theFile.name)+ '" onclick="showOps(this)" /\>');
+			};
+		})(f);
+		reader.readAsDataURL(f);
+		}
+		$('#newtagslist').html("");
+}
 function showImgOpts(that){
-	 theSrc = that.src;
-	 theTag = that.title;
-	
+	theSrc = that.src;
+	theTag = that.title;
 	var editImageDiv = '<div id="editImageDiv"><label>Edit Image Tags</label><br><textarea id="editTag">' + theTag + '</textarea><br><input type="hidden" id="srcVal" value="'+ theSrc +'"><input type="checkbox" id="chkbx1" name="chkbox1" value="Delete"  class="checkC" />Remove this image<br><input type="button" id="editTagsBtn" class="buttonclass" value="submit edit" onclick="markDel()" /></div>'  ;
-$('#oldTags').html(editImageDiv);
+	$('#oldTags').html(editImageDiv);
 }
-
 function markDel(){
- var thisFSrc = $('#srcVal').val();
- var chkBx = $('#chkbx1');
- var messg = "";
- var filepart = 'http://127.0.0.1/ibis/Data/Images/';//http://localhost/ibis/Data/Images/
- var localFpart = 'http://127.0.0.1/ibis/Data/Images/';//http://127.0.0.1/ibis/Data/Images/
- thisSrc = thisFSrc.replace(filepart, "");
- thisSrc = thisFSrc.replace(localFpart, "");
- alert(thisSrc);
- if ($('#chkbx1').prop('checked')) {
-
- //if (chkBx.checked == true){
- var imgDelList = thisSrc + ":";
-  var oldlist = $('#imgDeletelist').val();
-  var newList = oldlist + imgDelList;
-  $('#imgDeletelist').val(newList);
-  var imgList = document.images;
-  var imgCount = imgList.length;
-  for (i = 0; i < imgCount; i++){
-  	if (imgList[i].src == thisFSrc){
-  		imgList[i].src = "";
-  		imgList[i].title = "";
-  		
-  	}
-  }
- }else {
- var thisTag = $('#editTag').val();
- tagList += thisSrc + ":" + thisTag + "::";
- $('#editedtagslist').html(tagList);
-  var imgList = document.images;
-  var imgCount = imgList.length;
-  for (i = 0; i < imgCount; i++){
-  	if (imgList[i].src == thisFSrc){
-  		 imgList[i].title = thisTag;
-  		 
-  	}
-  }
- }
+	var thisFSrc = $('#srcVal').val();
+	var chkBx = $('#chkbx1');
+	var messg = "";
+	var localFpart = 'http://127.0.0.1/ibis/Data/Images/';//http://127.0.0.1/ibis/Data/Images/
+	thisSrc = thisFSrc.replace(localFpart, "");
+	if ($('#chkbx1').prop('checked')) {
+		var imgDelList = thisSrc + ":";
+	 	var oldlist = $('#imgDeletelist').val();
+	 	var newList = oldlist + imgDelList;
+		$('#imgDeletelist').val(newList);
+		var imgList = document.images;
+		var imgCount = imgList.length;
+		for (i = 0; i < imgCount; i++){
+			if (imgList[i].src == thisFSrc){
+				imgList[i].src = "";
+				imgList[i].title = "";
+		 	}
+		}
+	 }else {
+	 var thisTag = $('#editTag').val();
+	 tagList += thisSrc + ":" + thisTag + "::";
+	 $('#editedtagslist').val(tagList);
+		var imgList = document.images;
+		var imgCount = imgList.length;
+		for (i = 0; i < imgCount; i++){
+			if (imgList[i].src == thisFSrc){
+				 imgList[i].title = thisTag;
+		 	}
+		}
+	 }
 }
-
 function addTag(){
 	var tag = $('#mediatagsinput').val();
 	var tagpair = imgname +" : "+ tag + "::";
-	//var existingtags = " ";
 	var existingtags = $('#newtagslist').val();
 	if (existingtags.search(imgname) == -1){
 	var pretags = $('#newtagslist').val();
 	var newlist = tagpair + pretags;
-	//	var listitem = "<input type=\"text\" name=\"imgtag\" class=\"listitemC\" value=\""+newtag+"\" ></input><br />";
   $('#newtagslist').val(newlist);
 	}else{
-		alert("an entry for that allready exists" );
-			}
-
+		alert("an entry for that allready exists" );// should open dialog that allows edititing the tag
+	}
 }
 function showOps(that){
-		   	  imgname = that.title;
-		   	  $("#optionsDsplay").slideDown('fast');
-		   	  var opttext = '<p class="labelclass">Add Media Tags for <br>' + imgname + '</p><input type="text" name="AmediaTagsInput" id="mediatagsinput" class="littleDD inputclass" /><br><input type="button" class="buttonclass" value="Add" onclick="addTag();" />';
-		  		$('#optionsDsplay').html(opttext);
+	imgname = that.title;
+	$("#optionsDsplay").slideDown('fast');
+	var opttext = '<p class="labelclass">Add Media Tags for <br>' + imgname + '</p><input type="text" name="AmediaTagsInput" id="mediatagsinput" class="littleDD inputclass" /><br><input type="button" class="buttonclass" value="Add" onclick="addTag();" />';
+	$('#optionsDsplay').html(opttext);
 }
-
 function doSubmit(){
-document.EditsForm.submit();
+document.EditsForm.submit();// should validate and sanitize inputs here
 }
